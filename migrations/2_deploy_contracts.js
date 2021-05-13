4b00d4cc017e36174b39930e81c44d8c7989c9c0
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-05-13 14:52:51 +0200

    Adjust initial migration to fund airline and authorize app contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,21 @@
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
 const fs = require('fs');
+var BigNumber = require('bignumber.js');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
 
-    let firstAirlineAddress = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    let firstAirlineName = 'First Air';
+    let firstAirlineAddress = accounts[0];
+    let firstAirlineName = 'Testairline';
     deployer.deploy(FlightSuretyData, firstAirlineAddress, firstAirlineName)
     .then(() => {
         return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
-                .then(() => {
+                .then(async () => {
+                    const appInstance = await FlightSuretyApp.deployed();
+                    const dataInstance = await FlightSuretyData.deployed();
+                    await dataInstance.authorizeCaller(FlightSuretyApp.address);
+                    const weiMultiple = (new BigNumber(10)).pow(18);
+                    await appInstance.fundAirline(firstAirlineAddress, {from: firstAirlineAddress, value: 10*weiMultiple })
                     let config = {
                         localhost: {
                             url: 'http://localhost:8545',
