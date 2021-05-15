daffbefcd55b0d06b8a3f6f0138a29f4b174d6be
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-05-15 19:49:29 +0200

    First version of server code

--- src/server/server.js
+++ src/server/server.js
@@ -1,29 +1,95 @@
-import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
-import Config from './config.json';
-import Web3 from 'web3';
-import express from 'express';
+require("regenerator-runtime/runtime");
+import FlightSuretyApp from "../../build/contracts/FlightSuretyApp.json";
+import Config from "./config.json";
+import Web3 from "web3";
+import express from "express";
+var BigNumber = require("bignumber.js");
 
-
-let config = Config['localhost'];
-let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
+const weiMultiple = new BigNumber(10).pow(18);
+let config = Config["localhost"];
+let web3 = new Web3(
+  new Web3.providers.WebsocketProvider(config.url.replace("http", "ws"))
+);
 web3.eth.defaultAccount = web3.eth.accounts[0];
-let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+let numOracles = 2;
+let flightSuretyApp = new web3.eth.Contract(
+  FlightSuretyApp.abi,
+  config.appAddress
+);
 
+function getRandomStatusCode() {
+  const proportionalStatusCodes = [20, 20];
+  var code = proportionalStatusCodes[Math.floor(Math.random() * items.length)];
+  return code;
+}
 
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
+async function registerOracles() {
+  let oracleData = {};
+  let accounts = await web3.eth.getAccounts();
+  console.log("Accounts: ", accounts);
+  for (var i = 0; i < numOracles; i++) {
+    let account = accounts[i];
+    await flightSuretyApp.methods.registerOracle().send({
+      from: account,
+      value: 1 * weiMultiple,
+      gas: 1000000,
+    });
+    console.log("Registered Oracle at address: ", account);
+    let indexes = await flightSuretyApp.methods.getMyIndexes().call({
+      from: account,
+      gas: 1000000,
+    });
+    console.log("Got assigned indexes: ", indexes);
+    for(var index of indexes){
+      if(index in oracleData){
+        oracleData[index].push(account);
+      }
+      else {
+        oracleData[index] = [account];
+      }
+    }
+  }
+  return oracleData;
+}
+
+function processRequests(oracleData) {
+  flightSuretyApp.events.OracleRequest(
+    {
+      fromBlock: 0,
+    },
+    function (error, event) {
+      if (error) console.log(error);
+      else {
+        console.log("Oracle Request received for Index:", event.returnValues);
+        const index = event.returnValues.index;
+        if(index in oracleData){
+          const oracleAddresses = oracleData[index];
+          console.log(`Registered Oracles for index ${index}: `, oracleAddresses)
+          for(oracle in oracleAddresses){
+          }
+
+        }
+        else{
+          console.log("No oracles registered for requested index: ", index)
+        }
+
+      }
+    }
+  );
+}
+
+async function main() {
+  let oracleData = await registerOracles();
+  console.log("OracleData: ", oracleData);
+  processRequests(oracleData);
+}
+
+main();
+const app = express();
+app.get("/api", (req, res) => {
+  res.send({
+    message: "An API for use with your Dapp!",
+  });
 });
 
-const app = express();
-app.get('/api', (req, res) => {
-    res.send({
-      message: 'An API for use with your Dapp!'
-    })
-})
-
 export default app;
-
-
